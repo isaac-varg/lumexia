d7f9a5057d5507d07eb15a3ccf659c75188672d6
isaac-varg <hello@isaacvargas.dev>
2024-06-26 14:09:49 -0700

    Change default prisma seeds

--- prisma/seed/data/user.ts
+++ prisma/seed/data/user.ts
@@ -1,8 +1,8 @@
 export const users = [
     {
         id: '85c3a4f8-99e4-4fff-a6b6-c07f998d376a',
-        name: 'Woolrus',
-        email: 'woolrus@makicreations.com',
+        name: 'Lumexia',
+        email: "hello@isaacvargas.dev", 
         emailVerified: null,
         image: '',
     }
