afbd5ff546edcf2b51cdc6e2a432d5f6fe6c4a3f
isaac-varg <hello@isaacvargas.dev>
2024-08-07 16:13:57 -0700

    update dummy seed data

--- prisma/seed/data/alias.ts
+++ prisma/seed/data/alias.ts
@@ -1,10 +1,10 @@
 const aliases = [
-    {
-        id: '47d5c1d1-1120-4058-bbc9-8b17a8e26d6e',
-        itemId: '1794aae7-ee19-4763-b646-4842f318b458',
-        aliasTypeId: '7173f88e-057a-43a7-aa37-6b9b97707b08',
-        name: 'Leche de Almendra Deluxe 21',
-    }
+   // {
+   //     id: '47d5c1d1-1120-4058-bbc9-8b17a8e26d6e',
+   //     itemId: '1794aae7-ee19-4763-b646-4842f318b458',
+   //     aliasTypeId: '7173f88e-057a-43a7-aa37-6b9b97707b08',
+   //     name: 'Leche de Almendra Deluxe 21',
+   // }
 ]
 
 export default aliases
