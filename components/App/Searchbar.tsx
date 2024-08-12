0b4366452556ee5c27ac88221008a2092868dd59
isaac-varg <hello@isaacvargas.dev>
2024-08-12 09:33:51 -0700

    style searchbar

--- components/App/Searchbar.tsx
+++ components/App/Searchbar.tsx
@@ -1,23 +1,33 @@
 import { RiUserLine, RiSettingsLine } from "react-icons/ri";
 import UserIcon from "./UserIcon";
+import { BiSearchAlt } from "react-icons/bi";
+import { TbSettings } from "react-icons/tb";
+import { CommandMenu } from "../CommandMenu/CommandMenu";
 
 const Searchbar: React.FC = async () => {
 
   return (
-    <div className="flex items-center justify-between bg-gray-100 p-4 rounded-lg">
-      <div className="flex items-center">
-        <span className="text-gray-500 text-sm font-medium mr-4 font-poppins">CTRL + K</span>
+    <>
+      <CommandMenu />
+    <div className="flex items-center justify-between bg-neutral-100 p-4 rounded-lg">
+      <div className="flex gap-x-4 items-center text-neutral-400">
+        <span className="text-2xl"><BiSearchAlt /></span>
+        <span className="text-md font-medium font-poppins">CTRL + K</span>
         <input
           type="text"
           placeholder="Search..."
-          className="bg-transparent outline-none"
+          className="font-poppins font-medium text-md bg-transparent outline-none"
         />
       </div>
       <div className="flex items-center gap-x-4">
         <UserIcon />
-        <RiSettingsLine className="text-gray-700 text-2xl cursor-pointer" />
+        <div className="flex items-center justify-center bg-neutral-300 rounded-full w-8 h-8 p-1 hover:bg-neutral-400 hover:text-neutral-600 hover:cursor-pointer">
+          <TbSettings className="text-gray-500 text-2xl  " />
+
+          </div>
       </div>
     </div>
+    </>
   );
 };
 
---
+++ components/CommandMenu/CommandMenu.tsx
@@ -0,0 +1,1 @@
+// TODO use dialog to show cmdk menu
