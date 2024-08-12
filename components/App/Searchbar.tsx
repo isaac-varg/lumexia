f9918f964def35b2d02e86f34177b941b7325cb6
isaac-varg <hello@isaacvargas.dev>
2024-08-12 10:57:50 -0700

    fix build errors

--- components/App/Searchbar.tsx
+++ components/App/Searchbar.tsx
@@ -2,13 +2,11 @@ import { RiUserLine, RiSettingsLine } from "react-icons/ri";
 import UserIcon from "./UserIcon";
 import { BiSearchAlt } from "react-icons/bi";
 import { TbSettings } from "react-icons/tb";
-import { CommandMenu } from "../CommandMenu/CommandMenu";
 
 const Searchbar: React.FC = async () => {
 
   return (
     <>
-      <CommandMenu />
     <div className="flex items-center justify-between bg-neutral-100 p-4 rounded-lg">
       <div className="flex gap-x-4 items-center text-neutral-400">
         <span className="text-2xl"><BiSearchAlt /></span>
--- components/DataTable/SortableHeaderType.tsx
+++ components/DataTable/SortableHeaderType.tsx
@@ -1,15 +1,38 @@
+//"use client";
+//import SortableHead from "@/components/DataTable/SortableHead";
+//
+//
+//
+//export const SortableHeaderType = (title: string) => ({ column }: any) => {
+//  return (
+//    <div onClick={() => column.toggleSorting()}>
+//      <span className="flex flex-row items-center hover:cursor-pointer space-x-2">
+//        <div>{title}</div>
+//        <SortableHead sorted={column.getIsSorted()} />
+//      </span>
+//    </div>
+//  );
+//};
+//
+
 "use client";
 import SortableHead from "@/components/DataTable/SortableHead";
 
+export const SortableHeaderType = (title: string) => {
+  const Component = ({ column }: any) => {
+    return (
+      <div onClick={() => column.toggleSorting()}>
+        <span className="flex flex-row items-center hover:cursor-pointer space-x-2">
+          <div>{title}</div>
+          <SortableHead sorted={column.getIsSorted()} />
+        </span>
+      </div>
+    );
+  };
 
+  // Set the display name for the component
+  Component.displayName = `SortableHeaderType(${title})`;
 
-export const SortableHeaderType = (title: string) => ({ column }: any) => {
-  return (
-    <div onClick={() => column.toggleSorting()}>
-      <span className="flex flex-row items-center hover:cursor-pointer space-x-2">
-        <div>{title}</div>
-        <SortableHead sorted={column.getIsSorted()} />
-      </span>
-    </div>
-  );
+  return Component;
 };
+
