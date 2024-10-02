0a49cdaed313fa5796c126181478b6893738e601
isaac-varg <hello@isaacvargas.dev>
2024-10-02 15:43:16 -0700

    clean up seeds

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -4,9 +4,14 @@ import { seedAction } from "./seedAction";
 const main = async () => {
   await seedAction("inventoryType");
   await seedAction("procurementType");
-  await seedAction("user");
   await seedAction("itemType");
+
+  await seedAction("user");
+  await seedAction("account");
+
   await seedAction("item");
+  await seedAction("activityLog");
+
   await seedAction("containerType");
   await seedAction("transactionType");
   await seedAction("unitOfMeasurement");
@@ -14,19 +19,30 @@ const main = async () => {
   await seedAction("lot");
   await seedAction("container");
   await seedAction("transaction");
+
   await seedAction("aliasType");
   await seedAction("alias");
+
   await seedAction("supplier");
   await seedAction("purchaseOrderStatus");
   await seedAction("purchaseOrder");
   await seedAction("purchaseOrderItem");
+  await seedAction("purchaseOrderNote");
   await seedAction("supplierAlias");
   await seedAction("paymentMethod");
-  await seedAction("recordStatus")
-  await seedAction("stepAddendumType")
-  await seedAction("userRole")
-  await seedAction("bprStatus")
-  await seedAction("bprStagingStatus")
+  await seedAction("recordStatus");
+  await seedAction("stepAddendumType");
+  await seedAction("userRole");
+  await seedAction("supplierContact");
+  await seedAction("supplierContactNote");
+  await seedAction("supplierNote");
+  await seedAction("supplierPaymentMethod");
+
+  await seedAction("lotOrigin");
+  await seedAction("userRole");
+
+  await seedAction("bprStatus");
+  await seedAction("bprStagingStatus");
 };
 
 main()
