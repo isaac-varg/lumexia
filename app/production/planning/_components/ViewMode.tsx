b95875714d13fb2c176578be98082f3ded1a4441
Isaac Vargas <hello@isaacvargas.dev>
2024-12-09 16:28:20 -0800

    add calendar view to planning

--- app/production/planning/_components/ViewMode.tsx
+++ app/production/planning/_components/ViewMode.tsx
@@ -6,6 +6,7 @@ import { BprStatus } from "@/types/bprStatus"
 import ListMode from "./list/ListMode"
 import { PlanningIBpr } from "../_functions/getBprs"
 import ViewModeButton from "./ViewModeButton"
+import CalendarMode from "./calendar/CalendarMode"
 
 type ViewModeProps = {
     bprs: PlanningIBpr[]
@@ -13,21 +14,22 @@ type ViewModeProps = {
 
 }
 
-export type BprPlanningViewMode = "statusBoard" | "list"
+export type BprPlanningViewMode = "statusBoard" | "list" | "calendar"
 
 const ViewMode = ({ bprs, statuses }: ViewModeProps) => {
 
     const [viewMode, setViewMode] = useState<BprPlanningViewMode>("list")
-
 
     return (
         <div className="flex flex-col gap-y-6">
             <div className="flex items-center justify-start gap-x-4">
                 <ViewModeButton viewMode="list" onClick={setViewMode} activeViewMode={viewMode}>List</ViewModeButton>
                 <ViewModeButton viewMode="statusBoard" onClick={setViewMode} activeViewMode={viewMode}>Board</ViewModeButton>
+                <ViewModeButton viewMode="calendar" onClick={setViewMode} activeViewMode={viewMode}>Calendar</ViewModeButton>
             </div>
             {viewMode === 'list' && <ListMode bprs={bprs} />}
             {viewMode === 'statusBoard' && <StatusBoard bprs={bprs as any} statuses={statuses} />}
+            {viewMode === 'calendar' && <CalendarMode bprs={bprs} />}
         </div>
     )
 }
