e3738292882e561aaa41f76ab314fdb0186e4420
Isaac Vargas <hello@isaacvargas.dev>
2024-12-19 11:53:18 -0800

    modify audit to filter 0

--- app/inventory/audit/[lotId]/page.tsx
+++ app/inventory/audit/[lotId]/page.tsx
@@ -6,24 +6,24 @@ import Layout from "@/components/Layout";
 import BackButton from "./_components/BackButton";
 
 type AuditPageProps = {
-	params: {
-		lotId: string;
-	};
+    params: {
+        lotId: string;
+    };
 };
 
 const AuditPage = async ({ params }: AuditPageProps) => {
-	const lot = await lotActions.getOne(params.lotId);
-	const allLots = (await lotActions.getByItem(lot.itemId)) as any;
+    const lot = await lotActions.getOne(params.lotId);
+    const allLots = (await lotActions.getByItem(lot.itemId)) as any;
 
-	return (
-		<div className="flex flex-col gap-y-8">
-			<Layout.Row justify="between">
-			<PageTitle>Inventory Audit</PageTitle>
-				<BackButton />
-						</Layout.Row>
-			<AuditPanel allLots={allLots} />
-		</div>
-	);
+    return (
+        <div className="flex flex-col gap-y-8">
+            <Layout.Row justify="between">
+                <PageTitle>Inventory Audit</PageTitle>
+                <BackButton />
+            </Layout.Row>
+            <AuditPanel allLots={allLots} />
+        </div>
+    );
 };
 
 export default AuditPage;
