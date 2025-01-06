8a2a5cb2dcecc5ab97c9eae3145b150a6897a8fc
Isaac Vargas <hello@isaacvargas.dev>
2025-01-06 15:20:10 -0800

    add equipmentType for equipment

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -2,80 +2,83 @@
 import { seedAction } from "./seedAction";
 
 const main = async () => {
-        await seedAction("inventoryType");
-        await seedAction("procurementType");
-        await seedAction("itemType");
+    await seedAction("inventoryType");
+    await seedAction("procurementType");
+    await seedAction("itemType");
 
-        await seedAction("user");
-        await seedAction("verificationToken")
-        await seedAction("account");
+    await seedAction("user");
+    await seedAction("verificationToken")
+    await seedAction("account");
 
-        await seedAction("item");
-        await seedAction("activityLog");
+    await seedAction("item");
+    await seedAction("activityLog");
 
-        await seedAction("containerType");
-        await seedAction("transactionType");
-        await seedAction("unitOfMeasurement");
-        await seedAction("unitOfMeasurementConversion");
-        await seedAction("lot");
-        await seedAction("container");
-        await seedAction("transaction");
+    await seedAction("containerType");
+    await seedAction("transactionType");
+    await seedAction("unitOfMeasurement");
+    await seedAction("unitOfMeasurementConversion");
+    await seedAction("lot");
+    await seedAction("container");
+    await seedAction("transaction");
 
-        await seedAction("aliasType");
-        await seedAction("alias");
-        await seedAction("paymentMethod");
+    await seedAction("aliasType");
+    await seedAction("alias");
+    await seedAction("paymentMethod");
 
-        await seedAction("supplier");
-        await seedAction("purchaseOrderStatus");
-        await seedAction("purchaseOrder");
-        await seedAction("purchaseOrderItem");
-        await seedAction("purchaseOrderNote");
-        await seedAction("supplierAlias");
-        await seedAction("recordStatus");
-        await seedAction("stepAddendumType");
-        await seedAction("supplierContact");
-        await seedAction("supplierContactNote");
-        await seedAction("supplierNote");
-        await seedAction("supplierPaymentMethod");
+    await seedAction("equipment")
+    await seedAction("equipmentType")
 
-        await seedAction("userRole");
+    await seedAction("supplier");
+    await seedAction("purchaseOrderStatus");
+    await seedAction("purchaseOrder");
+    await seedAction("purchaseOrderItem");
+    await seedAction("purchaseOrderNote");
+    await seedAction("supplierAlias");
+    await seedAction("recordStatus");
+    await seedAction("stepAddendumType");
+    await seedAction("supplierContact");
+    await seedAction("supplierContactNote");
+    await seedAction("supplierNote");
+    await seedAction("supplierPaymentMethod");
 
-        await seedAction("bprStatus");
-        await seedAction("bprStagingStatus");
-        await seedAction("stepActionableType");
-        await seedAction("stepAddendumType")
+    await seedAction("userRole");
 
-        await seedAction("masterBatchProductionRecord");
-        await seedAction("batchStep");
-        await seedAction("stepActionable");
-        await seedAction("stepAddendum")
-        await seedAction("stepInstruction");
+    await seedAction("bprStatus");
+    await seedAction("bprStagingStatus");
+    await seedAction("stepActionableType");
+    await seedAction("stepAddendumType")
 
-        await seedAction("billOfMaterial");
-        await seedAction("batchSize");
-        await seedAction("stepEquipment")
-        await seedAction("bprStepActionableStatus");
-        await seedAction("bprBatchStepStatus");
+    await seedAction("masterBatchProductionRecord");
+    await seedAction("batchStep");
+    await seedAction("stepActionable");
+    await seedAction("stepAddendum")
+    await seedAction("stepInstruction");
 
-        await seedAction("batchProductionRecord");
-        await seedAction("bprBatchStep");
-        await seedAction("bprStepActionable");
-        await seedAction("bprBillOfMaterials");
-        await seedAction("bprStaging");
-        await seedAction("bprStagingVerification");
-        await seedAction("bprStepActionableCompletion");
+    await seedAction("billOfMaterial");
+    await seedAction("batchSize");
+    await seedAction("stepEquipment")
+    await seedAction("bprStepActionableStatus");
+    await seedAction("bprBatchStepStatus");
 
-        await seedAction("bprStepActionableVerification");
+    await seedAction("batchProductionRecord");
+    await seedAction("bprBatchStep");
+    await seedAction("bprStepActionable");
+    await seedAction("bprBillOfMaterials");
+    await seedAction("bprStaging");
+    await seedAction("bprStagingVerification");
+    await seedAction("bprStepActionableCompletion");
 
-        await seedAction("lotOrigin");
-        await seedAction("config")
-        //await seedAction("microSubmission");
+    await seedAction("bprStepActionableVerification");
+
+    await seedAction("lotOrigin");
+    await seedAction("config")
+    //await seedAction("microSubmission");
 };
 
 main()
-        .catch((e) => {
-                throw e;
-        })
-        .finally(async () => {
-                console.log("Seeding complete!");
-        });
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        console.log("Seeding complete!");
+    });
--- types/equipment.ts
+++ types/equipment.ts
@@ -2,6 +2,8 @@ export interface Equipment {
   id: string
   name: string
   identifier: string
+  equipmentTypeId: string
   createdAt: Date
   updatedAt: Date
 }
+
