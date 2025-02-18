7ac2949123be7201d4419f9f26beb00487c2e87b
Isaac Vargas <hello@isaacvargas.dev>
2025-02-18 10:04:47 -0800

    limit delivered cards on request; show cancelled as a count card instead of panel

--- app/purchasing/requests/_components/ByStatusPanel.tsx
+++ app/purchasing/requests/_components/ByStatusPanel.tsx
@@ -4,8 +4,9 @@ import { groupByProperty } from '@/utils/data/groupByProperty'
 import { RequestStatus } from '../[referenceCode]/_functions/getRequestStatuses';
 import RequestCard from './RequestCard';
 import { RequestPriority } from '../_functions/getPriorities';
+import { staticRecords } from '@/configs/staticRecords';
 
-const ByStatusPanel = ({ requests, statuses , priorities}: { requests: RequestForDashboard[], statuses: RequestStatus[], priorities: RequestPriority[] }) => {
+const ByStatusPanel = ({ requests, statuses, priorities }: { requests: RequestForDashboard[], statuses: RequestStatus[], priorities: RequestPriority[] }) => {
 
 
     const statusCounts = statuses.map((status) => ({
@@ -14,11 +15,28 @@ const ByStatusPanel = ({ requests, statuses , priorities}: { requests: RequestFo
     }));
 
 
+    
+    const canceledStatus = statusCounts[statusCounts.findIndex((s) => s.id === staticRecords.purchasing.requestStatuses.requestCancelledDuplicateRequest)]
+
+
+
+
     return (
         <div className='flex flex-col gap-y-6'>
 
             <div className='grid grid-cols-2 gap-6'>
                 {statusCounts.filter((s) => s.count !== 0).map((status) => {
+
+                    let requestsForStatus = requests.filter((req) => req.status.id === status.id);
+                    const isDelivered = status.id === staticRecords.purchasing.requestStatuses.delivered
+                    const isCancelled = status.id === staticRecords.purchasing.requestStatuses.requestCancelledDuplicateRequest
+
+                    if (isCancelled) { return }
+
+                    if (isDelivered) {
+                        requestsForStatus = requestsForStatus.slice(0, 10);
+                    }
+
                     return (
                         <div
                             key={status.id}
@@ -31,9 +49,9 @@ const ByStatusPanel = ({ requests, statuses , priorities}: { requests: RequestFo
                                     className='font-poppins text-xl font-semibold'>
                                     {status.name}
                                 </div>
-
+                                {isDelivered && <p>Only showing last 10</p>}
                                 <div className='grid grid-cols-3 max-h-80 overflow-y-auto gap-6'>
-                                    {requests.filter((req) => req.status.id === status.id).map((req) => <RequestCard key={req.id} statuses={statuses} priorities={priorities} request={req} />)}
+                                    {requestsForStatus.map((req) => <RequestCard key={req.id} statuses={statuses} priorities={priorities} request={req} />)}
                                 </div>
                             </div>
 
@@ -57,6 +75,15 @@ const ByStatusPanel = ({ requests, statuses , priorities}: { requests: RequestFo
                         </div>
                     )
                 })}
+
+                <div
+                    className='px-4 py-2 font-poppins text-sm font-semibold rounded-xl flex justify-between items-center'
+                    style={{ backgroundColor: canceledStatus.bgColor, color: canceledStatus.textColor }}
+                >
+                    <p>{canceledStatus.name}</p>
+                    <div className='rounded-full flex font-poppins text-sm text-black items-center justify-center h-12 w-12 bg-white' >{canceledStatus.count}</div>
+                </div>
+
 
             </div>
 
