21f281ef7ff03616c0e64d96bfef72d44f47785d
Isaac Vargas <hello@isaacvargas.dev>
2025-04-23 10:46:22 -0700

    initial sort suppliers; add audit request button
    - The suppliers table is now sorted alphabetically
    - An Audit request button has been added to the item details page

--- app/inventory/items/[name]/page.tsx
+++ app/inventory/items/[name]/page.tsx
@@ -12,57 +12,62 @@ import { ItemType } from "@/types/itemType";
 import { ProcurementType } from "@/types/procurementType";
 import { InventoryType } from "@/types/inventoryType";
 import { getAliases } from "./_functions/getAliases";
+import TopActions from "./_components/TopActions";
 
 type ItemDashboardProps = {
-  params: {
-    name: string;
-  };
-  searchParams: {
-    id: string;
-  };
+    params: {
+        name: string;
+    };
+    searchParams: {
+        id: string;
+    };
 };
 
 export type ItemEditSelectables = {
-  itemTypes: ItemType[]
-  procurementTypes: ProcurementType[]
-  inventoryTypes: InventoryType[]
+    itemTypes: ItemType[]
+    procurementTypes: ProcurementType[]
+    inventoryTypes: InventoryType[]
 }
 
 const ItemDashboard = async ({ params, searchParams }: ItemDashboardProps) => {
-  const item = await itemActions.getOne(searchParams.id, undefined, [
-    "itemType",
-    "procurementType",
-    "inventoryType",
-  ]);
-  
-  const itemTypes = await itemTypeActions.getAll()
-  const procurementTypes = await procurementTypeActions.getAll();
-  const inventoryTypes = await inventoryTypeActions.getAll();
+    const item = await itemActions.getOne(searchParams.id, undefined, [
+        "itemType",
+        "procurementType",
+        "inventoryType",
+    ]);
 
-  const itemEditSelectables: ItemEditSelectables = {
-    itemTypes,
-    procurementTypes,
-    inventoryTypes,
-  }
+    const itemTypes = await itemTypeActions.getAll()
+    const procurementTypes = await procurementTypeActions.getAll();
+    const inventoryTypes = await inventoryTypeActions.getAll();
+
+    const itemEditSelectables: ItemEditSelectables = {
+        itemTypes,
+        procurementTypes,
+        inventoryTypes,
+    }
 
 
-  const aliases = await getAliases(item.id) 
+    const aliases = await getAliases(item.id)
 
-  return (
-    <div className="flex flex-col gap-y-6">
-      <PageTitle title={item.name} />
+    return (
+        <div className="flex flex-col gap-y-6">
+            <div className="flex justify-between items-center">
+                <PageTitle title={item.name} />
 
-      <Layout.Grid>
-        <BasicsPanel item={item} itemEditSelectables={itemEditSelectables} />
+                <TopActions  itemId={item.id}/>
+            </div>
 
-        <AliasesPanel aliases={aliases} item={item} />
-      </Layout.Grid>
+            <Layout.Grid>
+                <BasicsPanel item={item} itemEditSelectables={itemEditSelectables} />
 
-      <TabsPanel item={item} />
+                <AliasesPanel aliases={aliases} item={item} />
+            </Layout.Grid>
 
-      
-    </div>
-  );
+            <TabsPanel item={item} />
+
+
+        </div>
+    );
 };
 
 export default ItemDashboard;
--- app/purchasing/suppliers/_components/Table.tsx
+++ app/purchasing/suppliers/_components/Table.tsx
@@ -6,29 +6,30 @@ import { Supplier } from "@/types/supplier";
 import { RowSelectionHandlerMethod, rowSelectionHandler } from "@/utils/auxiliary/rowSelectionHandler";
 
 type TableProps = {
-  suppliers: Supplier[];
+    suppliers: Supplier[];
 };
 
 const Table = ({ suppliers }: TableProps) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const handleRowClick = (row: any, method: RowSelectionHandlerMethod) => {
-    const formattedName = row.original.name.replace(/\s+/g, "-").toLowerCase();
-    const path = `/purchasing/suppliers/${`${formattedName}?id=${row.original.id}`} `
-    rowSelectionHandler(method, path, router)
-  };
+    const handleRowClick = (row: any, method: RowSelectionHandlerMethod) => {
+        const formattedName = row.original.name.replace(/\s+/g, "-").toLowerCase();
+        const path = `/purchasing/suppliers/${`${formattedName}?id=${row.original.id}`} `
+        rowSelectionHandler(method, path, router)
+    };
 
-  return (
-    <DataTable.Default
-      data={suppliers}
-      columns={columns}
-      //   filters={filters}
-      dialogIdentifier="createSupplier"
-      onRowClick={(row, method) => handleRowClick(row, method)}
-      onEnter={(row) => handleRowClick({ original: { ...row } }, 'rowClick')}
-      tableStateName="suppliers"
-    />
-  );
+    return (
+        <DataTable.Default
+            data={suppliers}
+            columns={columns}
+            //   filters={filters}
+            dialogIdentifier="createSupplier"
+            onRowClick={(row, method) => handleRowClick(row, method)}
+            onEnter={(row) => handleRowClick({ original: { ...row } }, 'rowClick')}
+            tableStateName="suppliers"
+            initialSortBy={[{ id: 'name', desc: false }]}
+        />
+    );
 };
 
 export default Table;
