6bb087769c724808050d1c15dd8a1b3b4a0fe1e2
Isaac Vargas <hello@isaacvargas.dev>
2025-06-23 15:52:45 -0700

    add ability to modify reference code in item dashboard

--- app/inventory/items/[name]/_components/basics/ItemEditForm.tsx
+++ app/inventory/items/[name]/_components/basics/ItemEditForm.tsx
@@ -13,63 +13,67 @@ import { ItemEditSelectables } from "../../page";
 import { createActivityLog } from "@/utils/auxiliary/createActivityLog";
 
 type Inputs = {
-  name: string
-  itemTypeId: string
-  procurementTypeId: string
-  inventoryTypeId: string
+    name: string
+    itemTypeId: string
+    procurementTypeId: string
+    inventoryTypeId: string
+    referenceCode: string
 };
 
-const ItemEditForm = ({item , itemEditSelectables } : { item: Item, itemEditSelectables: ItemEditSelectables } ) => {
-	const form = useForm<Inputs>({ defaultValues: {name: item.name, itemTypeId: item.itemTypeId, procurementTypeId: item.procurementTypeId, inventoryTypeId: item.inventoryTypeId } });
-	const {resetDialogContext } = useDialog();
+const ItemEditForm = ({ item, itemEditSelectables }: { item: Item, itemEditSelectables: ItemEditSelectables }) => {
+    const form = useForm<Inputs>({ defaultValues: { name: item.name, itemTypeId: item.itemTypeId, procurementTypeId: item.procurementTypeId, inventoryTypeId: item.inventoryTypeId, referenceCode: item.referenceCode } });
+    const { resetDialogContext } = useDialog();
 
 
-	const handleSubmit = async (data: Inputs) => {
-		await itemActions.update({id: item.id}, data);
-		
-    createActivityLog("modifyItem", "item", item.id, {context: `Modified item`})
-		revalidatePage("/inventory/items/[name]");
-		resetDialogContext();
-			
-	};
+    const handleSubmit = async (data: Inputs) => {
+        await itemActions.update({ id: item.id }, data);
+
+        createActivityLog("modifyItem", "item", item.id, { context: `Modified item` })
+        revalidatePage("/inventory/items/[name]");
+        resetDialogContext();
+
+    };
 
 
-	return (
-		<Dialog.Root identifier="editItem">
-			<Form.Root form={form} onSubmit={handleSubmit}>
-				<Form.Text form={form} label="Name" fieldName="name" required />
-			   <Form.Select
-          form={form}
-          fieldName="itemTypeId"
-          label="Item Type"
-          options={itemEditSelectables.itemTypes.map((it) => ({
-            value: it.id,
-            label: it.name,
-          }))}
-        />
- <Form.Select
-          form={form}
-          fieldName="procurementTypeId"
-          label="Procurement Type"
-          options={itemEditSelectables.procurementTypes.map((pt) => ({
-            value: pt.id,
-            label: pt.name,
-          }))}
-        />
- <Form.Select
-          form={form}
-          fieldName="inventoryTypeId"
-          label="Inventory Type"
-          options={itemEditSelectables.inventoryTypes.map((it) => ({
-            value: it.id,
-            label: it.name,
-          }))}
-        />
+    return (
+        <Dialog.Root identifier="editItem">
+            <Form.Root form={form} onSubmit={handleSubmit}>
+                <Form.Text form={form} label="Name" fieldName="name" required />
 
-				<Form.ActionRow form={form} />
-			</Form.Root>
-		</Dialog.Root>
-	);
+
+                <Form.Text form={form} label="Reference Code" fieldName="referenceCode" required />
+                <Form.Select
+                    form={form}
+                    fieldName="itemTypeId"
+                    label="Item Type"
+                    options={itemEditSelectables.itemTypes.map((it) => ({
+                        value: it.id,
+                        label: it.name,
+                    }))}
+                />
+                <Form.Select
+                    form={form}
+                    fieldName="procurementTypeId"
+                    label="Procurement Type"
+                    options={itemEditSelectables.procurementTypes.map((pt) => ({
+                        value: pt.id,
+                        label: pt.name,
+                    }))}
+                />
+                <Form.Select
+                    form={form}
+                    fieldName="inventoryTypeId"
+                    label="Inventory Type"
+                    options={itemEditSelectables.inventoryTypes.map((it) => ({
+                        value: it.id,
+                        label: it.name,
+                    }))}
+                />
+
+                <Form.ActionRow form={form} />
+            </Form.Root>
+        </Dialog.Root>
+    );
 };
 
 export default ItemEditForm
