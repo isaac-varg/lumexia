cce5e58b22899039bfdd6312d9f7662472a9ab54
Isaac Vargas <hello@isaacvargas.dev>
2025-08-13 11:04:35 -0700

    change styling of options

--- app/_components/panels/welcome/Welcome.tsx
+++ app/_components/panels/welcome/Welcome.tsx
@@ -3,17 +3,17 @@ import Panel from "../Panel"
 import otter from './otter.webp'
 
 const Welcome = () => {
-    return (
-        <Panel title="Dashboard Update">
-            <div className="flex flex-col gap-y-4">
-                <div className="flex justify-center items-center flex-col gap-y-4">
-                    <Image className=" rounded-full w-40 h-40" src={otter} alt="Otter" />
-                </div>
-                <p className="font-poppins">The data here should update without refreshing. Configuration and alert badges coming soon.</p>
-            </div>
+  return (
+    <Panel title="Dashboard Update">
+      <div className="flex flex-col gap-y-4">
+        <div className="flex justify-center items-center flex-col gap-y-4">
+          <Image className=" rounded-full w-40 h-40" src={otter} alt="Otter" />
+        </div>
+        <p className="font-poppins">Now with a theme switcher!</p>
+      </div>
 
-        </Panel>
-    )
+    </Panel>
+  )
 }
 
 export default Welcome
--- components/App/PageBreadcrumbs.tsx
+++ components/App/PageBreadcrumbs.tsx
@@ -1,12 +1,15 @@
 "use client"
 import React from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { TbArrowLeft, TbSlash } from "react-icons/tb";
 
 const PageBreadcrumbs = () => {
   const pathname = usePathname();
+  const searchParams = useSearchParams()
   const router = useRouter();
+  const id = searchParams.get('id')
   const segments = pathname.split("/").filter((segment) => segment !== "");
+  console.log(segments)
 
   return (
     <div className="flex flex-row space-x-2 text-cararra-600 items-center mb-4">
@@ -17,7 +20,7 @@ const PageBreadcrumbs = () => {
         const isLast = index === segments.length - 1;
         return (
           <React.Fragment key={index}>
-            <span><TbSlash className="text-2xl"/></span>
+            <span><TbSlash className="text-2xl" /></span>
             {isLast ? (
               <span className="font-poppins items-center text-xl font-medium text-cararra-600">
                 {segment}
