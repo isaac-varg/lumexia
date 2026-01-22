8c2e3b50c9778e9524e6c2593c7350f4e5f33ae8
Isaac Vargas <hello@isaacvargas.dev>
2026-01-22 15:14:34 -0800

    add complete starter functions

---
+++ app/accounting/pricing/[item]/new/_components/shared/CompleteButton.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+const CompleteButton = () => {
+
+  const handleComplete = async () => {
+
+  }
+
+  return (
+    <button
+      className="btn btn-success btn-lg"
+      onClick={handleComplete}
+    >
+      Complete
+    </button>
+
+  )
+}
+
+export default CompleteButton
--- app/accounting/pricing/[item]/new/_components/shared/Header.tsx
+++ app/accounting/pricing/[item]/new/_components/shared/Header.tsx
@@ -1,10 +1,12 @@
 'use client'
 import PageTitle from "@/components/Text/PageTitle"
 import { usePricingSharedSelection } from "@/store/pricingSharedSlice"
+import CompleteButton from "./CompleteButton"
 
 const Header = () => {
   const { item } = usePricingSharedSelection()
   if (!item) return false;
+
 
   return (
     <div className="flex justify-between items-center">
@@ -12,11 +14,7 @@ const Header = () => {
 
       <div className="flex gap-2">
 
-        <button
-          className="btn btn-success btn-lg"
-        >
-          Complete
-        </button>
+        <CompleteButton />
 
       </div>
 
