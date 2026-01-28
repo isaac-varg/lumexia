b8d4d366b77b9c347671803a1286f3467a1396e0
Isaac Vargas <hello@isaacvargas.dev>
2026-01-28 11:04:58 -0800

    fix build error of non-existant actionColor for alert dialog

--- app/accounting/pricing/[item]/new/_components/PricingErrorAlert.tsx
+++ app/accounting/pricing/[item]/new/_components/PricingErrorAlert.tsx
@@ -6,27 +6,27 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 const PricingErrorAlert = ({ error }: { error: string }) => {
-    const { showDialog } = useDialog()
-    const router = useRouter()
+  const { showDialog } = useDialog()
+  const router = useRouter()
 
-    useEffect(() => {
-        if (error) {
-            showDialog('pricingErrorAlert')
-        }
-    }, [error, showDialog])
+  useEffect(() => {
+    if (error) {
+      showDialog('pricingErrorAlert')
+    }
+  }, [error, showDialog])
 
-    return (
-        <Alert.Root identifier="pricingErrorAlert">
-            <Alert.Content
-                title="Pricing Error"
-                action={() => router.back()}
-                actionLabel="Go Back"
-                actionColor="danger"
-            >
-                {error}
-            </Alert.Content>
-        </Alert.Root>
-    )
+  return (
+    <Alert.Root identifier="pricingErrorAlert">
+      <Alert.Content
+        title="Pricing Error"
+        action={() => router.back()}
+        actionLabel="Go Back"
+        actionColor="error"
+      >
+        {error}
+      </Alert.Content>
+    </Alert.Root>
+  )
 }
 
 export default PricingErrorAlert
